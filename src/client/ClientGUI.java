3a8f631eeb7ae01681a6868e8ec6d0f294954341
Mike Shafer <mike.shafer2009@gmail.com>
2011-04-04 18:44:44 -0700

    made skeleton framework windows for app

--- src/client/ClientGUI.java
+++ src/client/ClientGUI.java
@@ -32,14 +32,57 @@ public class ClientGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        chatInput = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jSplitPane2 = new javax.swing.JSplitPane();
+        jSplitPane1 = new javax.swing.JSplitPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        messageFrame = new javax.swing.JTextArea();
+        jLayeredPane1 = new javax.swing.JLayeredPane();
         chatViewer = new javax.swing.JScrollPane();
         chatOutput = new javax.swing.JTextArea();
+        chatInput = new javax.swing.JTextField();
         sendButton = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jSplitPane3 = new javax.swing.JSplitPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        sourceTree = new javax.swing.JTree();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        codeFrame = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CodeBuddy Alpha");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18));
+        jLabel1.setText("CoderBuddy");
+
+        jSplitPane2.setDividerLocation(200);
+        jSplitPane2.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        jSplitPane2.setPreferredSize(new java.awt.Dimension(855, 474));
+
+        jSplitPane1.setDividerLocation(400);
+        jSplitPane1.setLastDividerLocation(400);
+        jSplitPane1.setPreferredSize(new java.awt.Dimension(855, 365));
+
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(400, 113));
+
+        messageFrame.setColumns(20);
+        messageFrame.setEditable(false);
+        messageFrame.setRows(5);
+        messageFrame.setPreferredSize(new java.awt.Dimension(400, 94));
+        jScrollPane1.setViewportView(messageFrame);
+
+        jSplitPane1.setLeftComponent(jScrollPane1);
+
+        chatViewer.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        chatViewer.setHorizontalScrollBar(null);
+
+        chatOutput.setColumns(20);
+        chatOutput.setEditable(false);
+        chatOutput.setLineWrap(true);
+        chatOutput.setRows(5);
+        chatViewer.setViewportView(chatOutput);
+
+        chatViewer.setBounds(0, 0, 450, 170);
+        jLayeredPane1.add(chatViewer, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         chatInput.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -51,15 +94,8 @@ public class ClientGUI extends javax.swing.JFrame {
                 chatInputKeyPressed(evt);
             }
         });
-
-        chatViewer.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        chatViewer.setHorizontalScrollBar(null);
-
-        chatOutput.setColumns(20);
-        chatOutput.setEditable(false);
-        chatOutput.setLineWrap(true);
-        chatOutput.setRows(5);
-        chatViewer.setViewportView(chatOutput);
+        chatInput.setBounds(10, 180, 360, 20);
+        jLayeredPane1.add(chatInput, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         sendButton.setLabel("Send");
         sendButton.addActionListener(new java.awt.event.ActionListener() {
@@ -67,35 +103,49 @@ public class ClientGUI extends javax.swing.JFrame {
                 sendButtonActionPerformed(evt);
             }
         });
+        sendButton.setBounds(380, 180, 70, 23);
+        jLayeredPane1.add(sendButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel1.setText("CoderBuddy");
+        jSplitPane1.setRightComponent(jLayeredPane1);
+
+        jSplitPane2.setRightComponent(jSplitPane1);
+
+        jSplitPane3.setDividerLocation(200);
+
+        sourceTree.setPreferredSize(new java.awt.Dimension(200, 200));
+        jScrollPane2.setViewportView(sourceTree);
+
+        jSplitPane3.setLeftComponent(jScrollPane2);
+
+        codeFrame.setColumns(20);
+        codeFrame.setRows(5);
+        codeFrame.setPreferredSize(new java.awt.Dimension(164, 200));
+        jScrollPane3.setViewportView(codeFrame);
+
+        jSplitPane3.setRightComponent(jScrollPane3);
+
+        jSplitPane2.setLeftComponent(jSplitPane3);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(chatInput, javax.swing.GroupLayout.DEFAULT_SIZE, 337, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(sendButton))
-            .addComponent(chatViewer, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(294, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addComponent(jSplitPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 865, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(chatViewer, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(chatInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(sendButton)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
+                .addComponent(jSplitPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -147,8 +197,18 @@ public class ClientGUI extends javax.swing.JFrame {
     private javax.swing.JTextField chatInput;
     private javax.swing.JTextArea chatOutput;
     private javax.swing.JScrollPane chatViewer;
+    private javax.swing.JTextArea codeFrame;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLayeredPane jLayeredPane1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JSplitPane jSplitPane2;
+    private javax.swing.JSplitPane jSplitPane3;
+    private javax.swing.JTextArea messageFrame;
     private javax.swing.JButton sendButton;
+    private javax.swing.JTree sourceTree;
     // End of variables declaration//GEN-END:variables
 
 }
